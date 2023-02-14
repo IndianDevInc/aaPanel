959374c4ca0382d09d786f52cee14de07bd19b64
Just_a_kartik <85619363+IndianDevInc@users.noreply.github.com>
2023-02-14 17:07:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,11 @@
-apt update -y
+sudo su
+sudo apt update -y
 
-# Install necessary packages
-apt -y install software-properties-common curl ca-certificates gnupg2 sudo lsb-release
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-# Add repository for PHP
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+sudo apt install 
 
-curl -fsSL  https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+sudo apt install wget
 
-# Add repository for Redis
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
-# Update  package lists
-apt update -y
-
-# Install PHP and required extensions
-apt install -y php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip}
-
-# MariaDB repo setup script
-curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-
-# Install the rest of dependencies
-apt install -y mariadb-server nginx tar unzip git redis-server
-
-
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
-
-curl -sSL https://get.docker.com/ | CHANNEL=stable bash
-
-systemctl enable --now docker
-
-apt install wget
-
-wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel
+sudo wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel
